9ba0b035d5ee10c2200e1a8b4e719b1b20a3a17b
Sertarus <certaryc@mail.ru>
2024-01-21 00:34:22 +0300

    voice rec added

--- main.py
+++ main.py
@@ -3,14 +3,28 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import speech_recognition as sr
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'H {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def voice_rec():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+    try:
+        return r.recognize_vosk(audio)
+    except sr.UnknownValueError:
+        print("Vosk could not understand audio")
+    except sr.RequestError as e:
+        print("Vosk error; {0}".format(e))
+
+
+def text_to_speech(text):
+    return text
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    text_to_speech(voice_rec())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
